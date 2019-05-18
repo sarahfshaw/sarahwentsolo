ed993eb37efc198f99542e408576d84f18f40688
sarah shaw <sarahfshaw@gmail.com>
2019-05-18 17:33:35 -0400

    new update
    updated code from saturday may 18th

---
+++ js/js.js
@@ -0,0 +1,11 @@
+$(function() {
+	$(".carousel").slick({
+		// dots: true,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		infinite: true,
+		fade: true,
+		cssEase: 'linear',
+		// arrows: false
+	});
+});
