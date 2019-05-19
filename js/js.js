d6728f235633f11d6a217600eefee624c2080eac
sarah shaw <sarahfshaw@gmail.com>
2019-05-18 20:50:09 -0400

    update saturday night
    added more blog pages

--- js/js.js
+++ js/js.js
@@ -8,4 +8,12 @@ $(function() {
 		cssEase: 'linear',
 		// arrows: false
 	});
+
+  $('#mobile-menu-button').click(function () {
+  if ($('.nav-container').hasClass('showing')) {
+    $('.nav-container').removeClass('showing').addClass('hiding')
+  } else {
+    $('.nav-container').removeClass('hiding').addClass('showing')
+  }
+})
 });
